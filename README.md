d01375e99bf0d1a431f916d9ecd11609f092c2cc
Katy Warren <katy.warren@bourne-leisure.co.uk>
2021-12-06 15:46:21 +0000

    day 2 part 2

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 [2021's Advent of Code](https://adventofcode.com/2021)
 
 Day 1: :star: 
-Day 2: :star:
+
+Day 2: :star: :star:
--- src/day2.js
+++ src/day2.js
@@ -6,6 +6,7 @@ class Accumulator {
     this.instructionList = instructions;
     this.horiz = 0;
     this.vert = 0;
+    this.aim = 0;
   }
 
   followInstruction = (line) => {
@@ -25,16 +26,44 @@ class Accumulator {
         console.log(`invalid operation ${operation}`);
         break;
     }
-    this.line++
+    this.line++;
   };
 
-  run = () => {
-    this.instructionList.forEach(line => {
-      this.followInstruction(line)
-    });
-    console.log(`program complete: horiz: ${this.horiz}, vert: ${this.vert}`)
-    return(this.horiz * this.vert) // we want the product
+  followInstructionPart2 = (line) => {
+    let [operation, value] = line.split(/\s/);
+    value = parseInt(value);
+    switch (operation) {
+      case "forward":
+        this.horiz += value;
+        this.vert += value * this.aim
+        break;
+      case "up":
+        this.aim -= value;
+        break;
+      case "down":
+        this.aim += value;
+        break;
+      default:
+        console.log(`invalid operation ${operation}`);
+        break;
+    }
+    this.line++;
+  };
 
+  run1 = () => {
+    this.instructionList.forEach((line) => {
+      this.followInstruction(line);
+    });
+    console.log(`program complete: horiz: ${this.horiz}, vert: ${this.vert}`);
+    return this.horiz * this.vert; // we want the product
+  };
+
+  run2 = () => {
+    this.instructionList.forEach((line) => {
+      this.followInstructionPart2(line);
+    });
+    console.log(`program complete: horiz: ${this.horiz}, vert: ${this.aim}`);
+    return this.horiz * this.vert;
   };
 }
 
@@ -46,4 +75,5 @@ const directions = fs
 
   const acc = new Accumulator(directions);
 
-  console.log(acc.run(directions))
+  //console.log(acc.run1(directions));
+  console.log(acc.run2(directions));
